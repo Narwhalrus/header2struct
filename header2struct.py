8f72000f982e16bc2e2f2ad55f1a06bc450bc4e1
Josh Riccobono <bh42877@simdev3.sim>
2016-07-01 11:02:41 -0500

    More work on typedef support.

--- header2struct.py
+++ header2struct.py
@@ -7,6 +7,7 @@ import json
 import struct
 from collections import OrderedDict
 from pprint import pprint
+import operator
 
 
 try:
@@ -14,6 +15,48 @@ try:
 except ImportError:
   sys.path.extend(['./pycparser', './pycparser/pycparser', './pycparserext', './pycparserext/pycparserext'])
   from pycparser import parse_file, c_parser, c_ast
+
+op_map = {
+  "*": operator.mul,
+  "+": operator.add,
+  "-": operator.sub,
+  "/": operator.div
+}
+
+c_type_map = {
+    'int': ctypes.c_int,
+    'unsigned int': ctypes.c_uint,
+    'unsigned': ctypes.c_uint,
+    'char': ctypes.c_byte,
+    'unsigned char': ctypes.c_ubyte,
+    'float': ctypes.c_float,
+    'double': ctypes.c_double,
+    'short': ctypes.c_short,
+    'unsigned short': ctypes.c_ushort
+}
+
+
+def resolve_binary_op(binary_op_obj):
+
+  op_fnc = op_map.get(binary_op_obj.op)
+  if op_fnc == None:
+    print('Could not find operator function for', binary_op_obj.op)
+    return 0
+
+  left = binary_op_obj.left
+  right = binary_op_obj.right
+
+  if isinstance(left, c_ast.BinaryOp):
+    left_val = resolve_binary_op(left)
+  else:
+    left_val = int(left.value)
+    
+  if isinstance(right, c_ast.BinaryOp):
+    right_val = resolve_binary_op(right)
+  else:
+    right_val = int(right.value)
+
+  return op_fnc(left_val, right_val)
   
 # This class takes a single Struct AST node and parses out all the fields
 class struct_def_generator(c_ast.NodeVisitor):
@@ -34,19 +77,35 @@ class struct_def_generator(c_ast.NodeVisitor):
       # If our decl is unnamed, just name it after the type
       name = node.type.name
 
+    print('PROCESSING', name)
+
     dims = []
     # TODO: Assumption: type will either be a TypeDecl (basic types and structs),
     # or an ArrayDecl.
-    type = node.type
+    field_type = node.type
     is_array = False
 
     # If type is an array decl then we have to loop through each
     # dimension of the array decl to get to TypeDecl (basic type or struct)
     # We'll also accumulate array dimensions
-    while isinstance(type, c_ast.ArrayDecl):
+    while isinstance(field_type, c_ast.ArrayDecl):
       is_array = True
-      dims.append(int(node.type.dim.value))
-      type = type.type
+      print('******************')
+      print(type(node.type.dim).__name__)
+      array_size = 0
+      # We have to resolve an expression in our array def
+      if isinstance(node.type.dim, c_ast.BinaryOp):
+        op = node.type.dim
+        print('Found binary op')
+        print(type(op.op), op)
+        # TODO: Remember to remove
+        array_size = resolve_binary_op(op)
+        print('Resolved binary op to', array_size)
+      else:
+        array_size = int(node.type.dim.value)
+        
+      dims.append(array_size)
+      field_type = field_type.type
     
     if not is_array:
       dims.append(1)
@@ -57,20 +116,20 @@ class struct_def_generator(c_ast.NodeVisitor):
     # Try testing with a typedef of a basic type and see what happens.
     # I'll probably have to keep track of all typedefs and resolve at some
     # point...
-    if isinstance(type, c_ast.TypeDecl):
-      type = type.type
+    if isinstance(field_type, c_ast.TypeDecl):
+      field_type = field_type.type
     else:
       print('I guess this doesnt have a type decl...')
 
     # Handle structs and basic types differently
-    if isinstance(type, c_ast.Struct) or isinstance(type, c_ast.Union):
+    if isinstance(field_type, c_ast.Struct) or isinstance(field_type, c_ast.Union):
       # If structure name is None, we're dealing with an anonymous struct
-      if type.name == None:
+      if field_type.name == None:
         typename = '<anonymous_struct>' 
       else:
-        typename = type.name
+        typename = field_type.name
     else:
-      typename = type.names
+      typename = field_type.names
       # a type 'unsigned x' would be stored as ['unsigned', 'x']
       # just flatten and parse out when generating ctypes struct
       typename = ' '.join(typename)
@@ -149,29 +208,40 @@ class struct_visitor(c_ast.NodeVisitor):
       self.visit(c)
     self.current_parent = old_parent
 
+
+class typedef_resolver(c_ast.NodeVisitor):
+  def __init__(self):
+    self.typedef_map = {}
+
+  def visit_Typedef(self, node):
+    print('**************************')
+    print ('IN TYPEDEF VISIT!')
+    print('**************************')
+    name = node.name
+    type_decl = node.type
+    print(type_decl.declname)
+    alias = type_decl.declname
+    if isinstance(type_decl.type, c_ast.IdentifierType):
+      typename = ' '.join(type_decl.type.names)
+      self.typedef_map[alias] = typename
+      print(alias,':',typename)
+
+    
+
 def generate_struct_defs(filename):
   ast = parse_file(filename, use_cpp=True)
   ast.show()
   sv = struct_visitor()
   sv.visit(ast)
+  
+  tdr = typedef_resolver()
+  tdr.visit(ast)
 
   
   parsed_structs = dict([(name, struct_def_generator(name).generate(node)) for name, node in sv.structs.iteritems()])
   parsed_unions = dict([(name, struct_def_generator(name).generate(node)) for name, node in sv.unions.iteritems()])
 
   return parsed_structs, parsed_unions
-
-c_type_map = {
-    'int': ctypes.c_int,
-    'unsigned int': ctypes.c_uint,
-    'unsigned': ctypes.c_uint,
-    'char': ctypes.c_byte,
-    'unsigned char': ctypes.c_ubyte,
-    'float': ctypes.c_float,
-    'double': ctypes.c_double,
-    'short': ctypes.c_short,
-    'unsigned short': ctypes.c_ushort
-}
 
 # TODO: Add alignment kwarg
 # TODO: With addition of Union type, names make less sense. Fix
